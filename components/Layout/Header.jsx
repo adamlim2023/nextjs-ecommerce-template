393fa1baad382bf48bff2b27d4c95364bf744740
Adam Lim <adamlim2023@gmail.com>
2023-04-27 18:48:02 -0700

    Added Header

---
+++ components/Layout/Header.jsx
@@ -0,0 +1,10 @@
+import ProfileIcon from '@/assets/images/icons/profile.svg';
+
+const Header = () => {
+  return <div>
+    <span>Home</span>
+    <img src={ProfileIcon} alt="" />
+  </div>
+}
+
+export default Header;
